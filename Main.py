eeb468b77c183cf30548eb1ae3573d2994db2eb1
Ashika35-wakanai <kaiashikaga06@gmail.com>
2025-12-10 22:12:16 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,44 @@
+import cv2
+from numpy.ma.core import negative
+
+cap = cv2.VideoCapture(0)
+faceFront = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("model.yml")
+
+
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frontFaceLine = faceFront.detectMultiScale(gray, 1.3, 5)
+
+
+    faces = list(frontFaceLine)
+    for (x, y ,w , h) in faces:
+        number, conf = recognizer.predict(gray[y:y+h, x:x+w])
+
+        positive = f"{"Buffe:"}  {conf}%"
+        negative = f"{"Unknown"} {conf}%"
+        if conf < 60:
+            cv2.putText(frame, positive, (x, y - 40), cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 255, 255), 2)
+            cv2.rectangle(frame, (x,y), (x + w, y+ h), (0, 165, 255), 5)
+
+        else:
+            cv2.putText(frame, negative, (x, y - 40), cv2.FONT_HERSHEY_TRIPLEX, 1, (50, 50, 255), 2)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+
+
+    cv2.imshow("Ako", frame)
+
+
+    if cv2.waitKey(1) == ord("d"):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
---
+++ Train_Model.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import os
+from PIL import Image
+
+path = "Buffe_Jp"
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+
+
+def getImageName(path):
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    faces = []
+    labels = []
+
+
+    for imagePath in imagePaths:
+
+
+        facePic = Image.open(imagePath).convert("L")
+        faceNp = np.array(facePic, 'uint8')
+
+        filename = os.path.basename(imagePath)
+        parts = filename.split(".")
+
+        label = int(parts[1])
+
+        faces.append(faceNp)
+        labels.append(label)
+
+        cv2.imshow("training...", faceNp)
+        cv2.waitKey(10)
+
+    return faces, labels
+
+
+
+faces, labels = getImageName(path)
+recognizer.train(faces, np.array(labels))
+recognizer.write("model.yml")
+cv2.destroyAllWindows()
+
+print("Faces:", len(faces), "Labels:", labels)
